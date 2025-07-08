1aa4ab36255288db7eb2bc0f4f233ee2eb484575
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-08 14:35:14 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
+
+//"homepage": "https://olehkozubjs.github.io/vue-gh-pages-test//"
 export default defineConfig({
   base: '/vue-gh-pages-test/',
   plugins: [vue(), vueDevTools()],
